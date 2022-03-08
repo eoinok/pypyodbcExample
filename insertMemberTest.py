d44fb27160ba95f96346f139d229cc1bf7a3d26e
Eoin OKennedy <Eoin.okennedy@tudublin.ie>
2022-03-08 12:55:09 +0000

    changed filename for insert and removed lock file

--- insertMemberTest.py
+++
@@ -1,9 +0,0 @@
-import pypyodbc
-DBfile = '.\\TennisClub.accdb'
-conn = pypyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+DBfile)
-myCursor = conn.cursor()
-SQL = "insert into member(firstname, surname, dateofbirth, membertype) values ('Eoin','OK','Senior','01/01/2010')"
-row = myCursor.execute(SQL)
-myCursor.commit()
-print("row inserted")
-
